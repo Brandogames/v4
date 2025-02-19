2b7d387db60edba2d0bf488db5df9ce3c113af96
Brandogames <97616011+Brandogames@users.noreply.github.com>
2025-02-19 16:42:42 -0500

    Update anym.js

--- static/assets/js/anym.js
+++ static/assets/js/anym.js
@@ -31,7 +31,7 @@
       document.addEventListener('keydown', function(event) {
         if (event.key === storedKey) {
           function redirect() {
-            document.write(`<script>window.location.href = 'https://verbose-rotary-phone-vjp77jg4755cpjxv-8000.app.github.dev/';</script>`);
+            document.write(`<script>window.location.href = 'https://reimagined-garbanzo-xgrqqg964p63px5q-8000.app.github.dev/';</script>`);
           }
           setInterval(redirect, 200);
         }
